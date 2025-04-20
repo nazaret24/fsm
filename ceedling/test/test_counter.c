4fea922e347a475a37be79b2e5c081dfbf4be621
Nazaret Pando <nazaret.pando.poblador@alumnos.upm.es>
2025-04-20 13:35:54 +0200

    Creación del módulo counter (vacío)

---
+++ ceedling/test/test_counter.c
@@ -0,0 +1,21 @@
+
+#ifdef TEST
+
+#include "unity.h"
+
+#include "counter.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_counter_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement counter");
+}
+
+#endif // TEST
